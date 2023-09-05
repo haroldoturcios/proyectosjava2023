be04a7981d467cbac5f1ae767524a4c5740caf34
haroldoturcios <haroldo.turcios@gmail.com>
2023-09-04 20:35:58 -0600

    proyecto subito a GitHub

---
+++ PIIB/src/PARCIAL_I/Ejercicio_1.java
@@ -0,0 +1,17 @@
+/*
+ Chimaltenango, 15 de julio de 2023.
+ Programador: Haroldo Turcios
+ Descripción del programa: Mi primer ejemplo en Java
+*/
+package PARCIAL_I; //Nombre del paquete
+public class Ejercicio_1 {//Inicio de la clase: Ejercicio_1
+   public static void main(String[] args){
+        System.out.println("Universidad Mariano Galvez");
+        System.out.println("Centro Universitario Chimaltenango");
+        System.out.println("Facultad de Ingeniería en Sistemas");
+        System.out.println("Curso Programación II");
+        System.out.println("Sección B");
+        System.out.println("Alumno: Haroldo Turcios");
+        System.out.println("Carnet: 0909334256");
+   }//fin del metodo main() 
+}//Fin de la clase Ejercicio_1
